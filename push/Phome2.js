7e70ed9b419992d6d9795816d3b47b999e96744a
Deimo <102623297+Dorgu77@users.noreply.github.com>
2022-09-19 16:48:15 -0700

    Add files via upload

---
+++ push/Phome2.js
@@ -0,0 +1,127 @@
+
+ import React from 'react'
+ import {useState} from 'react'
+ import "./Port22.css"
+ 
+ 
+
+
+
+
+    export default function Phome2 (){
+
+  
+
+
+
+return(
+<div>
+
+<div id="container">
+<div id="main">
+    
+    <nav id="n2">
+  <ul>  
+<li><a href="Portfolio.js" target="blank">Home</a></li>
+
+<li><a href="Portfolio.js" target="blank">portfolio</a></li>
+</ul>
+    
+    </nav>
+<div id="plate2">
+    <h2 id="h1" >..portfolio</h2>
+    <div id="ft1">Hello, Am </div>
+    <h1 id="h2">DEIMO DORGU</h1>
+    <div id="h3">Am a senior frontend web and mobile developer..</div>
+    <img   id="use2" src="usepic.png"/>
+
+
+
+    <span id="net1">
+    <a target="_blank" href="https://www.ggogle.com"><span className="rot1">hire  me</span></a>
+    <a target="_blank" href="https://www.ggogle.com"><span className="rot1">portfolio</span></a>
+</span>
+</div>
+
+
+
+
+
+
+
+<h1 id="ho2">Acquired Skills</h1>
+
+<div className="gr2">
+<img src="html.png"/>
+<img src="css.png"/>
+<img src="JavaScript.png"/>
+<img src="logo512.png"/>
+<img src="JavaScript.png"/>
+</div>
+
+<div>
+
+
+<div id="lit">
+    <ul>  
+<li><h7 id="n4">About me</h7></li>
+<li id="n10"> Am dorgu deimo, a front end web designer and developer based in Nigeria.</li>
+</ul>
+
+<ul id="u3">
+    <li><h8 id="n5">contact me</h8></li>
+    <li>mobile : 08167693412</li>
+    <li>Address : no 36 erepa road yenagoa</li>
+    <li>Email : deimodorgutech@gmail.com</li>
+    </ul>  
+</div>
+
+    
+
+</div>
+
+ <h3>Experiences</h3>
+ 
+  <h4>Front end developer</h4>
+  <div id="mid">
+ Three years of experience in freelance 
+ and fulltime jobs... 
+ </div>
+
+
+
+ <span id="proj">..portfolio projects</span>
+<div className="im2">
+<a target="_blank" href="#"><img alt="project1" src="laaap.png"/></a>
+<a target="blank" href="#"><img alt="project2"  src="usepic.png"/></a>
+<a target="_blank" href="#"><img  alt="project3" src="paath.png"/></a>
+<a target="_blank" href="#"><img alt="project4" src="music.png"/></a>
+<a target="_blank" href="#"><img alt="project5" src="physic.png"/></a>
+<a target="_blank" href="#"><img alt="project6" src="traffic.png"/></a>
+</div>
+
+
+
+
+</div>
+</div>
+    
+<footer id="footer">  
+<div id="pag2">copyright write </div>
+<span className="sos2">
+        <ul className="lu2">
+        <a  target="_blank" href="https://www.ggogle.com"><li><img   src="WhatsApp.png" /></li></a>
+        <a  target="_blank" href="https://www.ggogle.com"><li><img   src="instagram.png" /></li></a>
+        <a  target="_blank" href="https://www.ggogle.com"><li><img   src="twitter.png" /></li></a>
+        <a  target="_blank" href="https://www.ggogle.com"><li><img   src="email.png" /></li></a>
+         </ul>
+    </span>
+    <div id="cpag2">Dorgu Deimo page</div>
+    
+</footer>
+
+</div>
+);
+}
+
+
