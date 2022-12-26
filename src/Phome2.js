a8ab4437dacef62d42707cf3618e9937d59d301d
Deimo <102623297+Dorgu77@users.noreply.github.com>
2022-12-26 09:53:13 -0800

    Add files via upload

--- src/Phome2.js
+++ src/Phome2.js
@@ -1,18 +1,11 @@
 
  import React from 'react'
  import {useState} from 'react'
- 
  import {Link} from 'react-router-dom';
- 
- 
-
-
-
 
     export default function Phome2 (){
 
   
-
         const[ind, setInd] =React.useState({company:"", role:"", country:"", description:"", stacks:"", type:""}) 
         function gas(event){
             setInd(prevInd=>{
@@ -54,19 +47,56 @@ return(
 </div>
 
 
-<h1 id="skills">Acquired Skills</h1>
+<h4 id="skills">Our Services</h4>
 
 <div className="skillbox">
-<div><span id="html">Html</span><img src="html.png"/></div>
-<div><span id="css">Css</span><img src="css.png"/></div>
-<div><span id="javascript">Javascript</span><img src="JavaScript.png"/></div>
-<div><span id="react">React.js</span><img src="react.png"/></div>
-<div><span id="node">Node.js</span><img src="node.png"/></div>
-<div><span id="express">Express.js</span><img src="express.png"/></div>
+
+
+<div> <img src="frontend.png" /><h5 className="stacks">Front end</h5><div className="stackmid">we design and develop scalable frontend layouts from idea stage to product level..
+Our engineers are equiped with skills like..<br/>
+<div style={{textAlign:"center"}}><br/>
+<b>Javascript <br/>
+Css <br/>
+Html<br/>
+React.js</b>
+</div>
+</div>
+</div>
+
+<div> <img src="backend.png" /><h5 className="stacks">Back end</h5><div className="stackmid">we design and develop scalable back end APIs and servers from idea stage to product level..
+Our engineers are equiped with skills like..<br/><br/>
+<div style={{textAlign:"center"}}>
+<b>Node.js <br/>
+Express <br/>
+</b>
+</div></div></div>
+
+<div> <img src="database.png" /><h5 className="stacks">DataBase</h5><div className="stackmid">we design and develop scalable frontend layouts from idea stage to product level..
+Our engineers are equiped with skills like..<br/>
+<div style={{textAlign:"center"}}>
+<b><br/>
+Mysq<br/>
+MongoDB<br/></b>
+</div>
+</div></div>
+
+
+<div> <img src="mobile.png" /><h5 className="stacks">Mobile</h5><div className="stackmid">we design and develop scalable frontend layouts from idea stage to product level,
+products are bulit for both android and ios platforms..
+Our engineers are equiped with skills like..<br/>
+<div style={{textAlign:"center"}}>
+<b><br/>
+React native<br/>
+Kotlin
+<br/></b>
+</div>
+</div></div>
+
 </div>
 
 <br/><br/>
 <div id="contact1">
+
 <div className="exper">
  <h3>Experiences</h3>
 Front end developer
@@ -76,26 +106,29 @@ Front end developer
 
 <br/>
 <div id="inputs">
-<a  id="hir"><span id="soap">Contact form</span></a>
+<a  id="hir"><span className="stackmid">Contact form</span></a>
                 <form action="https://www.formsubmit.co/deimodorgutech@gmail.com" method="post">
-            <input placeholder="company name"  style={{color:"orange", fontSize:"20px"}} size="34" className="py" name="company" required type="text"onChange={gas}   value={ind.company} /><br/><br/>
-            <input  placeholder="role"  style={{color:"orange", fontSize:"20px"}} size="34" type="text"  className="py" name="role"  required onChange={gas}  value={ind.role} /><br/><br/>
-            <input type="text" placeholder="country" style={{color:"orange", fontSize:"20px"}} size="34" name="country" className="py"required onChange={gas} value={ind.country}  /><br/><br/>
+            <input placeholder="company name"  style={{color:"orange", fontSize:"20px"}} size="20" className="py" name="company" required type="text"onChange={gas}   value={ind.company} /><br/><br/>
+            <input  placeholder="role"  style={{color:"orange", fontSize:"20px"}} size="20" type="text"  className="py" name="role"  required onChange={gas}  value={ind.role} /><br/><br/>
+            <input type="text" placeholder="country" style={{color:"orange", fontSize:"20px"}} size="20" name="country" className="py"required onChange={gas} value={ind.country}  /><br/><br/>
 
-            <input placeholder="stacks needed" style={{color:"orange", fontSize:"20px"}} size="34" name="stacks" className="py" required type="text"onChange={gas}   value={ind.stacks} /><br/>
+            <input placeholder="stacks needed" style={{color:"orange", fontSize:"20px"}} size="20" name="stacks" className="py" required type="text"onChange={gas}   value={ind.stacks} /><br/>
 <br/>
-            <select   className="boss" style={{color:"orange", padding:"5px"}}  className="py" name="type" onChange={gas} valu={ind.type}>
+            <select   className="boss" style={{color:"black", padding:"5px"}}   name="type" onChange={gas} valu={ind.type}>
                 <option>select job type</option>
                 <option>fully onsite</option>
                 <option>fully remote</option>
             </select>
 <br/>
 <br></br>
-        <button type="submit"  style={{color:"orange", padding:"6px"}}  className="py">submit</button>
-            </form></div>
+        <button type="submit"  style={{color:"black", padding:"6px"}}  className="py">submit</button> </form> <h4 style={{opacity:"0.7"}}>Or</h4> <a  target="blank" href="mailto:deimoddorgutech@gmail.com"><button  style={{color:"black",  padding:"6px"}}  className="py">Send mail</button></a>
+        
+           
+            
+</div>
          <br/>   
 <div className="exper">
-<a id="ab"> <h3 id="n4">About me</h3></a>
+<a id="ab"> <h3>About me</h3></a>
     Am dorgu deimo, a front end web designer and developer based in Nigeria.<br/>
     mobile: 08167693412 <br/>
     Address: no 36 erepa road yenagoa<br/>
@@ -109,15 +142,19 @@ Front end developer
 
 
      <br/>
-<a id="tah"><div id="projhead">..portfolio projects</div></a>
+<a id="tah"><div id="projhead">..Recent projects</div></a>
 <div className="projectbox">
 
-<a  target="blank" href=""><div><span id="project1">project1</span><img  src="laaap.png"/></div></a>
-<a  target="blank" href="#"><div><span id="project2">project2</span><img  src="usepic.png"/></div></a>
-<a  target="blank" href="#"><div><span id="project3">project3</span><img  src="paath.png"/></div></a>
-<a  target="blank" href="#"><div><span id="project4">project4</span><img  src="coder.png"/></div></a>
-<a  target="blank" href="#"><div><span id="project5">project5</span><img  src="physic.png"/></div></a>
-<a  target="blank" href="#"><div><span id="project6">project6</span><img  src="travel.png"/></div></a>
+<a  target="blank" href="
+https://play.google.com/store/apps/details?id=baap.app&referrer=utm_source%3Dapps.facebook.com%26utm_campaign%3Dfb4a%26utm_content%3D%257B%2522app%2522%253A0%252C%2522t%2522%253A1671969462%252C%2522source%2522%253Anull%257D
+
+"><div><span id="project1">Baap app</span><img  src="baap.png"/></div></a>
+<a  target="blank" href="https://play.google.com/store/apps/details?id=com.dr_recruiter"><div><span id="project2">Dr recruiter</span><img  src="dr.png"/></div></a>
+<a  target="blank" href="http://13.235.74.134"><div><span id="project3">Bike site</span><img  src="wep.png"/></div></a>
+<a  target="blank" href="https://www.rainforestcafe.com"><div><span id="project4">forest cafe</span><img  src="rainforest.png"/></div></a>
+<a  target="blank" href="https://quizninja.netlify.app/#/"><div><span id="project5">   Quiz ninja</span><img  src="ninja.png"/></div></a>
+<a  target="blank" href="https://hoteltask.com/hoteltask/public/login
+"><div><span id="project6">Hotel booking</span><img  src="hotel.png"/></div></a>
 </div>
 
 
@@ -125,14 +162,14 @@ Front end developer
 <br/>
 
 <div className="social">
-        <a  target="_blank" href="https://www.facebook.com/dorgu.deimo"><img src="Facebook.png" /></a>
-      <a  target="_blank" href="https://www.instagram.com/p/CVcgPRWMQWO/?igshid=YmMyMTA2M2"><img   src="instagram.png" /></a>
-      <a  target="_blank" href="https://twitter.com/Deimo27279785/status/1570176314971136002?s=20&t=pQrF2a9T6Zo42IUUJtmCbA"><img   src="twitter.png" /></a>
-      <a  target="_blank" href="mailto:deimoddorgutech@gmail.com"><img   src="email.png" /></a>
+        <a  target="_blank" href="https://www.facebook.com/dorgu.deimo"><img src="reatface.png" /></a>
+      <a  target="_blank" href="https://www.instagram.com/p/CVcgPRWMQWO/?igshid=YmMyMTA2M2"><img src="realinsta.png" /></a>
+      <a  target="_blank" href="https://twitter.com/Deimo27279785/status/1570176314971136002?s=20&t=pQrF2a9T6Zo42IUUJtmCbA"><img src="realtwit.png" /></a>
+      <a  target="_blank" href="mailto:deimoddorgutech@gmail.com"><img src="realwhats.png" /></a>
        
 </div>
 <div id="deimo">Dorgu Deimo page</div>
-<div id="copywrite">@copywrite</div>
+<div id="copywrite">@copywrite All rights reserved..</div>
 </footer>
 
 </div>
